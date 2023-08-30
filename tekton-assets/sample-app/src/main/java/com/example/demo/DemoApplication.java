b6d0c5d8de88125ef28200aef71c4240ee0366c1
ggalloro <galloro.giovanni@gmail.com>
2023-08-30 15:34:02 +0200

    Testing new sample app

--- tekton-assets/sample-app/src/main/java/com/example/demo/DemoApplication.java
+++ tekton-assets/sample-app/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package io.buildpacks.example.sample;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SampleApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SampleApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
